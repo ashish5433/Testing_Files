2c867e298224aa484fad46a509d0e8beb607631f
milind-palaria <lit2021047@iiitl.ac.in>
2024-05-04 07:21:04 +0530

    Config File Fix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  optimizeFonts: false,
   
   images: {
     domains: ['firebasestorage.googleapis.com','flagcdn.com'],
-    optimizeFonts: false,
 
 },
 }
