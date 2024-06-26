1469dedf4e28362d867f72551e9dd06387dfcbb9
milind-palaria <lit2021047@iiitl.ac.in>
2024-06-26 14:50:05 +0530

    Spin Loader till Material Icons loaded

--- pages/_app.js
+++ pages/_app.js
@@ -1,35 +1,50 @@
 import '@/styles/globals.css'
 import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
+// import { useRouter } from 'next/router';
 import Loading from '@/components/loading';
+import FontFaceObserver from 'fontfaceobserver';
 
+import Router from 'next/router';
 export default function App({ Component, pageProps }) {
-
   const [loading, setLoading] = useState(true);
-  const router = useRouter();
 
   useEffect(() => {
     const handleRouteChangeStart = () => setLoading(true);
     const handleRouteChangeComplete = () => setLoading(false);
 
-    router.events.on('routeChangeStart', handleRouteChangeStart);
-    router.events.on('routeChangeComplete', handleRouteChangeComplete);
-    router.events.on('routeChangeError', handleRouteChangeComplete);
+    Router.events.on('routeChangeStart', handleRouteChangeStart);
+    Router.events.on('routeChangeComplete', handleRouteChangeComplete);
+    Router.events.on('routeChangeError', handleRouteChangeComplete);
 
-    // Hide loading screen when the component mounts
-    handleRouteChangeComplete();
+    // Ensure the loading screen stays until Material Icons are loaded
+    const loadMaterialIcons = () => {
+      return new Promise((resolve) => {
+        const materialIconsObserver = new FontFaceObserver('Material Symbols Outlined', { weight: 100 });
+
+        materialIconsObserver.load(null, 10000).then(() => {
+          resolve();
+        }).catch(() => {
+          resolve(); // Resolve even if it fails to prevent getting stuck
+        });
+      });
+    };
+
+    const onLoad = async () => {
+      await loadMaterialIcons();
+      handleRouteChangeComplete();
+    };
+
+    onLoad(); // Trigger the initial load check
 
     return () => {
-      router.events.off('routeChangeStart', handleRouteChangeStart);
-      router.events.off('routeChangeComplete', handleRouteChangeComplete);
-      router.events.off('routeChangeError', handleRouteChangeComplete);
+      Router.events.off('routeChangeStart', handleRouteChangeStart);
+      Router.events.off('routeChangeComplete', handleRouteChangeComplete);
+      Router.events.off('routeChangeError', handleRouteChangeComplete);
     };
-  }, [router]);
-
-
+  }, []);
   return (
     <>
-      {loading && <Loading />}
+    {loading && <Loading />}
       <Component {...pageProps} />
     </>
   );
--- pages/_document.js
+++ pages/_document.js
@@ -4,9 +4,13 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-          <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-      </Head>
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
+          <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+          {/* <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" /> */}
+      </Head>
+          {/* <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" /> */}
+      
       <link href="https://api.fontshare.com/v2/css?f[]=clash-grotesk@300,500&display=swap" rel="stylesheet"/>
       <link
         rel="stylesheet"
@@ -14,7 +18,7 @@ export default function Document() {
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
         crossOrigin="anonymous"
       />
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+      
       <link href="https://api.fontshare.com/v2/css?f[]=cabinet-grotesk@100,300,200&display=swap" rel="stylesheet"/>
       <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
       <script src="https://www.gstatic.com/charts/loader.js"></script>
