ad469d8597fa28c2f0216d3bff1aea926a6fc502
milind-palaria <lit2021047@iiitl.ac.in>
2024-06-26 13:02:53 +0530

    Loading Functionality #1

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,36 @@
 import '@/styles/globals.css'
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Loading from '@/components/loading';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChangeStart = () => setLoading(true);
+    const handleRouteChangeComplete = () => setLoading(false);
+
+    router.events.on('routeChangeStart', handleRouteChangeStart);
+    router.events.on('routeChangeComplete', handleRouteChangeComplete);
+    router.events.on('routeChangeError', handleRouteChangeComplete);
+
+    // Hide loading screen when the component mounts
+    handleRouteChangeComplete();
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChangeStart);
+      router.events.off('routeChangeComplete', handleRouteChangeComplete);
+      router.events.off('routeChangeError', handleRouteChangeComplete);
+    };
+  }, [router]);
+
+
+  return (
+    <>
+      {loading && <Loading />}
+      <Component {...pageProps} />
+    </>
+  );
 }
