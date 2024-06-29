c131dfe3a7f9d393eb18c78f8cf314964b99f2e0
milind-palaria <lit2021047@iiitl.ac.in>
2024-06-29 07:58:02 +0530

    login page initialization and fixes

--- pages/login/index.js
+++ pages/login/index.js
@@ -2,18 +2,22 @@ import Image from "next/image";
 import classes from "../../styles/Login.module.css";
 import SignIn from "@/components/signIn";
 import SignUp from "@/signs/signUp3";
+import VideoBackground from "@/components/VideoBackground";
 // import SignIn from "../../signs/signIn2";
 // import SignUp from "../../signs/signUp3";
 export default function LoggingIn() {
   return (
       <div>
-      <div className={classes.main_div}>
+        <VideoBackground>
+          
+        </VideoBackground>
+      {/* <div className={classes.main_div}>
         <div className={classes.login_div}>
           <SignIn />
         </div>
         <div className={classes.signup_div}>
           <SignUp />
-        </div>
+        </div> */}
         {/* <div className={classes.assistance_btn}>
           <Image
             className={`${classes.assist_icon}`}
@@ -22,8 +26,8 @@ export default function LoggingIn() {
             height={50}
           />
           <p>Seek Assistance For Account Creation</p>
-        </div> */}
-      </div>
+        </div>
+      </div> */}
     </div>
   );
 }
