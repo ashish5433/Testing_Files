9e2f57089c9f4f26ae345c8fecd3dfcd4f07c3c9
milind-palaria <lit2021047@iiitl.ac.in>
2024-07-11 23:51:00 +0530

    Border Change

--- components/Login-sections/SignUp.js
+++ components/Login-sections/SignUp.js
@@ -175,7 +175,7 @@ const SignUp = () => {
                 {/* <h1>Register</h1> */}
                     <input
                         type="text"
-                        className="bg-black/40 text-white border-0 p-2  focus:bg-black/60 focus:outline-none focus:ring-1 focus:ring-[#4dffb5]/50 transition ease-in-out duration-150 w-full"
+                        className="bg-black/40 text-white border !border-[#4dffb5]/10 focus:!border-[#4dffb5]/60 p-2  focus:bg-black/60 focus:!rounded-none transition ease-in-out duration-150 w-full"
                         placeholder="Full Name" onChange={(e) => {
                             setregisterName(e.target.value);
                         }} />
