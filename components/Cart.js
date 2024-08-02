4d448022c2697cc51cab0620884f1c0888bb8dad
milind-palaria <lit2021047@iiitl.ac.in>
2024-08-03 00:09:23 +0530

    Cart Modifications #2

--- components/Cart.js
+++ components/Cart.js
@@ -38,20 +38,20 @@ const CartPage = ({ showModal, onClose }) => {
 
     return (
         <Modal show={showModal} onClose={onClose}>
-        <h1 className={bebas.className}>Your Cart</h1>
+        <h1 className={`${bebas.className} border-b-2`}>Your Cart</h1>
         {cart.length === 0 ? (
           <p>Your cart is empty</p>
         ) : (
-          <ul>
+          <ul className="p-0 m-0 h-full w-full flex justify-center items-center">
             {cart.map(item => (
-              <li key={item.id} className={`${unica.className} flex items-center justify-center flex-col`}>
-                <h2>{item.productName}</h2>
-                <p>Price: ${item.productPrice}</p>
+              <li key={item.id} className={`${unica.className} flex items-center justify-center flex-col mt-1 w-full`}>
+                <h2 className="text-[340%] leading-0 mb-0">{item.productName}</h2>
+                <p className="text-[120%]">Price: ${item.productPrice}</p>
                 <div className="flex">
 
-                <p>Quantity: {item.quantity}</p>
-                <button onClick={() => updateQuantity(item.id, item.quantity - 1)}>-</button>
-                <button onClick={() => updateQuantity(item.id, item.quantity + 1)}>+</button>
+                <p className="text-[120%]">Quantity: {item.quantity}</p>
+                <button className=" text-[280%] leading-[0] ml-4 -translate-y-[20%] bg-white rounded-full text-black p-[0.7rem]" onClick={() => updateQuantity(item.id, item.quantity - 1)}>-</button>
+                <button className=" text-[280%] leading-[0] ml-4 -translate-y-[20%] bg-white rounded-full text-black p-[0.7rem]" onClick={() => updateQuantity(item.id, item.quantity + 1)}>+</button>
                 </div>
               </li>
             ))}
